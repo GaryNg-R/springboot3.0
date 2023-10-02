1aaa6f1c6a3d8120b8c295beca57e967bd590082
Gary Ng <garyng@garys-mbp.attlocal.net>
2023-10-01 20:23:46 -0700

    init project

--- src/main/java/com/spring/learning/learningspringframework/LearningSpringFrameworkApplication.java
+++ src/main/java/com/spring/learning/learningspringframework/LearningSpringFrameworkApplication.java
@@ -1,5 +1,7 @@
 package com.spring.learning.learningspringframework;
 
+import com.spring.learning.learningspringframework.game.GameRunner;
+import com.spring.learning.learningspringframework.game.MarioGame;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +10,9 @@ public class LearningSpringFrameworkApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LearningSpringFrameworkApplication.class, args);
+		MarioGame game = new MarioGame();
+		GameRunner runner = new GameRunner(game);
+		runner.run();
 	}
 
 }
---
+++ src/main/java/com/spring/learning/learningspringframework/game/MarioGame.java
@@ -0,0 +1,4 @@
+package com.spring.learning.learningspringframework.game;
+
+public class MarioGame {
+}
