db510cba9ebb7e27b04c25f67a05b643c49614af
Gary Ng <garyng@garys-mbp.attlocal.net>
2023-10-04 11:28:05 -0700

    using spring annotation to create bean and get bean to create object

--- src/main/java/com/spring/learning/learningspringframework/LearningSpringFrameworkApplication.java
+++ src/main/java/com/spring/learning/learningspringframework/LearningSpringFrameworkApplication.java
@@ -3,16 +3,21 @@ package com.spring.learning.learningspringframework;
 import com.spring.learning.learningspringframework.game.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class LearningSpringFrameworkApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LearningSpringFrameworkApplication.class, args);
-		//GamingConsole game = new SuperContraGame();
+		ConfigurableApplicationContext context = SpringApplication.run(LearningSpringFrameworkApplication.class, args);
+
+		// GamingConsole game = new SuperContraGame();
 		//GamingConsole game = new MarioGame();
-		GamingConsole game = new PacmanGame();
-		GameRunner runner = new GameRunner(game);
+		//GamingConsole game = new PacmanGame();
+		//GameRunner runner = new GameRunner(game);
+		//runner.run();
+		GameRunner runner = context.getBean(GameRunner.class);
+
 		runner.run();
 	}
 
--- src/main/java/com/spring/learning/learningspringframework/game/MarioGame.java
+++ src/main/java/com/spring/learning/learningspringframework/game/MarioGame.java
@@ -1,5 +1,8 @@
 package com.spring.learning.learningspringframework.game;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class MarioGame implements GamingConsole {
 
     public void up(){
