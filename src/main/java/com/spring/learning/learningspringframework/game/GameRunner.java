c9e0d91d7ec328043274a3355ffe7d1cfa4c4001
Gary Ng <garyng@garys-mbp.attlocal.net>
2023-10-04 09:37:37 -0700

    tightly coupled

--- src/main/java/com/spring/learning/learningspringframework/game/GameRunner.java
+++ src/main/java/com/spring/learning/learningspringframework/game/GameRunner.java
@@ -1,10 +1,15 @@
 package com.spring.learning.learningspringframework.game;
 
 public class GameRunner {
-    public GameRunner(MarioGame game) {
 
+    private MarioGame game;
+
+    public GameRunner(MarioGame game) {
+        this.game = game;
     }
 
     public void run() {
+        game.up();
+        game.down();
     }
 }
