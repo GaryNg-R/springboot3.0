5774618e745b94161c2c4f46f0545b1038ca0baa
Gary Ng <garyng@garys-mbp.attlocal.net>
2023-11-04 12:30:31 -0700

    move folder

---
+++ dummyspring/src/main/java/com/dummy/springboot/dummyspring/DummywebappApplication.java
@@ -0,0 +1,13 @@
+package com.dummy.springboot.dummyspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DummywebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DummywebappApplication.class, args);
+	}
+
+}
---
+++ dummyspring/src/test/java/com/dummy/springboot/dummyspring/DummywebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dummy.springboot.dummyspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DummywebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
